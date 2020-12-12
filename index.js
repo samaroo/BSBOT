c0d63e06dd3bbbfeb7e5a7ee398c538ca80c37a7
Kevin Qi <kevinqi@utexas.edu>
2020-12-12 01:35:25 -0600

    added welcome message and adding/removing roles WIP

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
 // Import the discord.js module
-const Discord = require('discord.js');
-const token = require('./token');
-const basicToolbox = require('./basicToolbox');
+const Discord = require("discord.js");
+const token = require("./token");
+const basicToolbox = require("./basicToolbox");
+const kevinToolbox = require("./kevinToolbox");
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
 // char to trigger bot command
-const trigger = '!';
+const trigger = "!";
 
-client.once('ready', () => {
-    console.log('BSBOT Online!');
-})
+client.once("ready", () => {
+  console.log("Einstien Online!");
+});
 
-client.on('message', message => {
-    //if the message is not meant for the bot, or the bot itself wrote it, ignore it
-    if(!message.content.startsWith(trigger) || message.author.bot) { return; }
+client.on("guildMemberAdd", (member) => {
+  kevinToolbox.sendWelcomeMsg(member);
+});
 
-    //get actual message without "!"
-    const msg = message.content.slice(trigger.length).toLowerCase();
+client.on("message", (message) => {
+  //if the message is not meant for the bot, or the bot itself wrote it, ignore it
+  if (!message.content.startsWith(trigger) || message.author.bot) {
+    return;
+  }
 
-    switch(msg){
-        case 'future':
-            basicToolbox.futurePong(message);
-            break;
-        
-        case 'introduce':
-            basicToolbox.introduce(message);
-            break;
-    }
+  // Split by spaces to better determine commands
+  const words = message.content.split(" ");
 
-})
+  //get actual message without "!"
+  const msg = words[0].slice(trigger.length).toLowerCase();
+
+  switch (msg) {
+    case "future":
+      basicToolbox.futurePong(message);
+      break;
+
+    case "introduce":
+      basicToolbox.introduce(message);
+      break;
+
+    case "addrole":
+      kevinToolbox.assignRole(message, words[1]);
+      break;
+
+    case "removerole":
+      kevinToolbox.removeRole(message, words[1]);
+      break;
+  }
+});
 
 //generate token from outside fucntion to keep token safe
 const secret = token.generateToken();
