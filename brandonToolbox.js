34c9f37c9cc17fbe519197d0facc5724a7d9dc2b
samaroo <samaroo@umich.edu>
2020-12-19 16:29:18 -0500

    Fixed invite gen bug

--- brandonToolbox.js
+++ brandonToolbox.js
@@ -4,7 +4,11 @@ const reactRoll = () => {
 
 const generateInvite = async (messageObj) => {
     let channel = messageObj.channel;
-    let invite = await channel.createInvite({unique: true, maxAge: 172800, maxUses: 1});
+    let invite = await channel.createInvite({
+        unique: true,
+        maxAge: 86400,
+        maxUses: 1
+    });
     messageObj.channel.send("Pls don't be dumb with this... https://discord.gg/" + invite.code);
 };
 
