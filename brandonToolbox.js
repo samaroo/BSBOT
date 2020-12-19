e3fe01709585c03dad33eb8dd7d4b6110cca734b
samaroo <samaroo@umich.edu>
2020-12-19 12:57:13 -0500

    capped maxUses to 1 for invites

--- brandonToolbox.js
+++ brandonToolbox.js
@@ -4,7 +4,7 @@ const reactRoll = () => {
 
 const generateInvite = async (messageObj) => {
     let channel = messageObj.channel;
-    let invite = await channel.createInvite({unique: true, maxAge: 172800});
+    let invite = await channel.createInvite({unique: true, maxAge: 172800, maxUses: 1});
     messageObj.channel.send("Pls don't be dumb with this... https://discord.gg/" + invite.code);
 };
 
