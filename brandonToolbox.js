2d29a990b2cc2106e29be0dd57ae63c1a51aff18
samaroo <samaroo@umich.edu>
2020-12-19 03:46:56 -0500

    Made invites generated with Einstein last only 2 days

--- brandonToolbox.js
+++ brandonToolbox.js
@@ -4,7 +4,7 @@ const reactRoll = () => {
 
 const generateInvite = async (messageObj) => {
     let channel = messageObj.channel;
-    let invite = await channel.createInvite({unique: true});
+    let invite = await channel.createInvite({unique: true, maxAge: 172800});
     messageObj.channel.send("Pls don't be dumb with this... https://discord.gg/" + invite.code);
 };
 
